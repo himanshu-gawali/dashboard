ed6f97f3a5387194ee6617dbc3f639d1e7d9f65d
Himanshu Gawali <himanshu.sandip.gawali@gmail.com>
2023-11-29 12:28:33 +0530

    git init

---
+++ creative-fuel project/project/js/index.js
@@ -0,0 +1,11 @@
+
+
+function menuFunction(){
+    var menu = document.getElementById("sidebar")
+    menu.style.display = "flex";
+}
+
+function closeFunction(){
+    var menu = document.getElementById("sidebar")
+    menu.style.display = "none";
+}
